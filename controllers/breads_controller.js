ac7f28fdbb953671154f567c7254e72d0359acaf
Michael Kawata <michaelkawata@bens-air.lan>
2023-01-25 20:35:29 -0800

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -29,10 +29,12 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
-// DELETE
-breads.delete('/:indexArray', (req, res) => {
-    Bread.splice(req.params.indexArray, 1)
-    res.status(303).redirect('/breads')
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray
+    })
 })
 
 // SHOW
@@ -45,6 +47,23 @@ breads.get('/:arrayIndex', (req, res) => {
     } else {
         res.render('404')
     }
+})
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+    Bread.splice(req.params.indexArray, 1)
+    res.status(303).redirect('/breads')
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 module.exports = breads
