bf13007af8a0712bbf6761d01d5506b2debc2445
Michael Kawata <michaelkawata@bens-air.lan>
2023-02-11 11:54:18 -0800

    fix

--- models/bread.js
+++ models/bread.js
@@ -1,32 +1,41 @@
 // require mongoose 
 const mongoose = require('mongoose')
+const breads = require('../controllers/breads_controller')
 // creating shorthand for the Schema constructor 
 const { Schema } = mongoose
 
-// schema
+
 const breadSchema = new Schema({
     name: { type: String, required: true },
     hasGluten: Boolean,
-    image: { type: String, default: 'http://placehold.it/500x500.png' },
+    image: { type: String, default: 'https://via.placeholder.com/500' },
     baker: {
         type: Schema.Types.ObjectID,
         ref: 'Baker'
     }
 })
 
-// helper methods 
+// Instance helper methods 
 breadSchema.methods.getBakedBy = function () {
-    return `${this.name} was baked with love by ${this.baker.name}, who has been with us since ${this.baker.startDate.getFullYear()}`
+    return `${this.name} was baked with love by ${this.baker.name}, 
+  who has been with us since ${this.baker.startDate.getFullYear()}`
 }
 
 breadSchema.methods.getBakerBio = function () {
     return `Baker Biography: ${this.baker.bio}`
 }
 
+// Static helper method
+
+// breadSchema.static.getBreadsByBaker = function(bakerName){
+//   return this.find({baker: bakerName})
+// }
+
 breadSchema.static('getBreadsByBaker', function (bakerName) {
     return this.find({ baker: bakerName })
 })
 
-// model and export 
 const Bread = mongoose.model('Bread', breadSchema)
+
+
 module.exports = Bread
